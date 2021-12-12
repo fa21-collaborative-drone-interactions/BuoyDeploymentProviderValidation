dcb57c515cf8ebce3aee8cefe8fd2812bde3eb0f
Felix Desiderato <felix.desiderato@tum.de>
2021-12-12 23:07:37 +0100

    Adjustments

--- Sources/BuoyDeploymentOption/BuoyDeploymentOption.swift
+++ Sources/BuoyDeploymentOption/BuoyDeploymentOption.swift
@@ -1,4 +1,12 @@
 import Apodini
+//
+// This source file is part of the FA2021 open source project
+//
+// SPDX-FileCopyrightText: 2019-2021 Paul Schmiedmayer and the project authors (see CONTRIBUTORS.md) <paul.schmiedmayer@tum.de>
+//
+// SPDX-License-Identifier: MIT
+//
+
 import ApodiniDeployBuildSupport
 import DeploymentTargetIoTCommon
 import Foundation
